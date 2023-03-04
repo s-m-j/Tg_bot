429c5dbbec1e2715d4d367545134b6b739976761
Sergey Maslov <sergey.s.maslov@gmail.com>
2023-03-04 21:06:35 +0300

    add state

--- handlers.py
+++ handlers.py
@@ -2,6 +2,8 @@ from aiogram.types import Message, ReplyKeyboardRemove, CallbackQuery
 from aiogram.dispatcher.filters import Text, Command
 from keyboards import keyboard, keyboard1, phone_key, mac_key
 from main import bot, dp
+from aiogram.dispatcher import FSMContext
+from shop import Shop, price1, price2
 
 from config import chat_id
 
@@ -42,5 +44,40 @@ async def macbook(call: CallbackQuery):
 @dp.callback_query_handler(text_contains='cancel')
 async def cancel(call: CallbackQuery):
     await call.answer('Отмена', show_alert=True)
-
     await call.message.edit_reply_markup(reply_markup=None)
+
+
+# Section: ShopApp
+@dp.message_handler(Command('buy'), state=None)
+async def shop(message: Message):
+    await message.answer('Какой товар вы хотите купить 1 или 2')
+    await Shop.step1.set()
+
+
+@dp.message_handler(state=Shop.step1)
+async def shop(message: Message, state: FSMContext):
+    item = message.text
+
+    await state.update_data(
+        {
+            'item': item
+        }
+    )
+
+    await message.answer('Сколько?')
+    await Shop.next()
+
+
+@dp.message_handler(state=Shop.step2)
+async def count(message: Message, state: FSMContext):
+    data = await state.get_data()
+    item = data.get('item')
+    if item == '1':
+        p = price1
+    else:
+        p = price2
+
+    count = int(message.text)
+
+    await message.answer(f'Отлично! с вас {p * count}')
+    await state.finish()
--- main.py
+++ main.py
@@ -1,13 +1,19 @@
 import asyncio
 
 from aiogram import Bot, Dispatcher, executor
+from aiogram.contrib.fsm_storage.memory import MemoryStorage
 
 from config import BOT_TOKEN
 
 loop = asyncio.new_event_loop()
 bot = Bot(BOT_TOKEN, parse_mode='HTML')
-dp = Dispatcher(bot, loop=loop)
+storage = MemoryStorage()
+dp = Dispatcher(bot, loop=loop, storage=storage)
 
+
+async def shutdown(dp):
+    await storage.close()
+    await bot.close()
 
 if __name__ == '__main__':
     from handlers import dp, send_hello
